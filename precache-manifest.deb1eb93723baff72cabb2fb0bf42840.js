aae6dfc3c0f9c9fcd29fa47a153121a96214b6ed
Shridhar <shridharelango@gmail.com>
2020-09-02 18:38:18 +0530

    Updates

---
+++ precache-manifest.deb1eb93723baff72cabb2fb0bf42840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f184a66989e6f46feb106142fecffc83",
+    "url": "/tetris-game/index.html"
+  },
+  {
+    "revision": "bf04b261697388fda225",
+    "url": "/tetris-game/static/css/main.7d7deb05.chunk.css"
+  },
+  {
+    "revision": "de10b577660ef3b7457b",
+    "url": "/tetris-game/static/js/2.3cb05dad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tetris-game/static/js/2.3cb05dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf04b261697388fda225",
+    "url": "/tetris-game/static/js/main.a5c4fa1f.chunk.js"
+  },
+  {
+    "revision": "4e54b2446b4bbe560a75",
+    "url": "/tetris-game/static/js/runtime-main.7f86b2a8.js"
+  },
+  {
+    "revision": "bdf4302ce21870a3255975fb63e8e1ec",
+    "url": "/tetris-game/static/media/dark.bdf4302c.jpeg"
+  }
+]);
